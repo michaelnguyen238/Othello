5e9664f3fa55d37bf8580ae481216775ec718840
mike13m <michael.nguyen238@gmail.com>
2015-04-16 20:35:01 -0700

    Initial Commit

---
+++ Project 2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project 2
+//
+//  Created by Michael Nguyen on 4/16/15.
+//  Copyright (c) 2015 Michael Nguyen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+    return 0;
+}
